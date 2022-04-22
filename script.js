512f1bde0f4f36d8e555c5d35609da4c1df1fc4e
Samar BAGHDADI <samar.baghdadi@gmail.com>
2022-04-22 09:23:41 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function Logout(element) {
+  element.innerText = "Logout";
+}
+function removeBtn(element) {
+  element.remove();
+}
+function alertThis(element) {
+  console.log("Ninja was liked");
+  alert("Ninja was liked");
+}
